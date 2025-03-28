dcedc40fa6b34ad144d3dc35aadbdfa0cdd5d703
vempadasiva <vempadasivakumarreddy@gmail.com>
2025-03-28 12:13:26 +0530

    updated version by chatgpt

--- calculator_app.py
+++ calculator_app.py
@@ -1,22 +1,48 @@
-def calculator(start):
-    
+import sys
+
+def calculator(start, input1=None, input2=None, operation=None):
     while start:
-        operation = input('enter which type of operation you need: add/sub/mul/div/stop: ')
+        if not operation:
+            operation = input('Enter which type of operation you need: add/sub/mul/div/stop: ')
+
         if operation == 'stop':
             start = False
             continue
-        input1 = int(input('enter a number:'))
-        input2 = int(input('enter other number: '))
+
+        if input1 is None:
+            input1 = int(input('Enter a number: '))  # First number
+
+        if input2 is None:
+            input2 = int(input('Enter another number: '))  # Second number
+
         if operation == 'add':
-            print('Add of given numbers: ', input1+input2)
+            print('Addition of given numbers:', input1 + input2)
         elif operation == 'sub':
-            print('substract of given numbers: ', input1-input2)
+            print('Subtraction of given numbers:', input1 - input2)
         elif operation == 'mul':
-            print('Multiply of given numbers: ', input1*input2)
+            print('Multiplication of given numbers:', input1 * input2)
         elif operation == 'div':
-            print('division of given numbers: ',input1/input2)
-        
+            print('Division of given numbers:', input1 / input2)
         else:
-            return ' onvalid operaton'
+            print('Invalid operation')
+            return
+
+        # Ask for operation again or stop
+        operation = input('Enter which type of operation you need: add/sub/mul/div/stop: ')
+
 if __name__ == '__main__':
-    calculator(start=True)
+    # Check if there are command-line arguments
+    if len(sys.argv) > 1:
+        # Extract values from command-line arguments
+        try:
+            operation = sys.argv[1]  # First argument
+            input1 = int(sys.argv[2])  # Second argument
+            input2 = int(sys.argv[3])  # Third argument
+            calculator(start=True, input1=input1, input2=input2, operation=operation)
+        except IndexError:
+            print("Please provide operation, input1, and input2 as arguments.")
+        except ValueError:
+            print("Please ensure input1 and input2 are integers.")
+    else:
+        # Default case: Run interactively
+        calculator(start=True)
